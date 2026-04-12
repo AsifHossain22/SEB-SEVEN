757d7a85db4b0a1a8174516f971e7362f24907a2
AsifHossain22 <only1asif4you22@gmail.com>
2026-04-12 12:06:03 +0400

    Explored 'Sorting and Flattening Array'

---
+++ Be A Critical Thinker in JavaScript/JavaScript Data Transformation and Aggregation/sortingAndFlatteningArray.js
@@ -0,0 +1,32 @@
+// Sort
+const numbers = [40, 100, 1, 5, 25, 10];
+const fruits = ["Banana", "apple", "Cherry", "date"];
+
+const sortedNumber = numbers.sort((a, b) => b - a); // DescendingOder // InPlaceSorting
+// console.log(numbers);
+// console.log(sortedNumber);
+
+fruits.sort((a, b) => a.localeCompare(b)); // StringSortingInAlphabeticalOder
+// console.log(fruits);
+
+// NestedArrayFlat
+
+const arr = [1, 2, 3, [4, 5, [6, 7, [8, 9, [10, 11]]]]];
+
+const flatArr = arr.flat(Infinity);
+// console.log(flatArr);
+
+const tagsFromPosts = [
+  ["javascript", "react", "css"],
+  ["node", "express"],
+  ["css", "html", "react"],
+];
+
+const filterTags = [...new Set(tagsFromPosts.flat())];
+console.log(filterTags);
+
+// Some
+const currentUserRoles = ["user", "editor"];
+const featureAccessRoles = ["admin", "manager"];
+
+// Array.from()
