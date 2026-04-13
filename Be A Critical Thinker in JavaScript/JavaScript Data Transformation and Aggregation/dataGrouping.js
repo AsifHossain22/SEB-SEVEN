ade87b9b79f919f76cf5d388085fc9b77225422e
AsifHossain22 <only1asif4you22@gmail.com>
2026-04-13 17:21:26 +0400

    Explored 'Data Grouping'

---
+++ Be A Critical Thinker in JavaScript/JavaScript Data Transformation and Aggregation/dataGrouping.js
@@ -0,0 +1,36 @@
+//* GroupingAndAggregatingData
+
+//? input
+const surveyResponses = [
+  "A",
+  "C",
+  "B",
+  "A",
+  "B",
+  "B",
+  "C",
+  "A",
+  "B",
+  "D",
+  "A",
+  "C",
+  "B",
+  "A",
+];
+
+const count = surveyResponses.reduce((table, response) => {
+  //   console.log(table, ":", response);
+  //   if (table[response]) {
+  //     table[response] = table[response] + 1;
+  //   } else {
+  //     table[response] = 1;
+  //   }
+
+  table[response] = (table[response] || 0) + 1;
+
+  return table;
+}, {});
+console.log(count);
+
+//? Output
+// { A: 5, C: 3, B: 5, D: 1 }
