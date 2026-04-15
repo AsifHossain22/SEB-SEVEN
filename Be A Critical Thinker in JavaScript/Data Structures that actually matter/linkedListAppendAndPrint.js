37626052680af5f61ce377af4d3f51d90e26e050
AsifHossain22 <only1asif4you22@gmail.com>
2026-04-15 15:34:09 +0400

    Explored 'Insert'

--- Be A Critical Thinker in JavaScript/Data Structures that actually matter/linkedListAppendAndPrint.js
+++ Be A Critical Thinker in JavaScript/Data Structures that actually matter/linkedListAppendAndPrint.js
@@ -24,13 +24,71 @@ class LinkedList {
     }
 
     this.length++;
+
+    return this;
   }
 
-  prepend() {}
+  prepend(value) {
+    const newNode = new Node(value);
+    // IfTheLinkedListEmpty
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // IfTheLinkedListNotEmpty
+      newNode.next = this.head;
+      this.head = newNode;
+    }
 
-  insert() {}
+    this.length++;
+
+    return this;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      console.error("Index out of bound: Shohoj Bangla Bhashay Muri Kha");
+      return undefined;
+    }
+
+    // IfTheInsertIsStartOfTheLinkedList
+    if (index === 0) {
+      return this.prepend(value);
+    }
+
+    // IfTheInsertIsInTheEndOfTheList
+    if (index === this.length) {
+      return this.append(value);
+    }
+
+    // IfTheInsertIsInTheMiddle
+    const leadingNode = this._traverseToIndex(index - 1);
+    // console.log(leadingNode);
+
+    const holdingNode = leadingNode.next;
+
+    const newNode = new Node(value);
+
+    leadingNode.next = newNode;
+
+    newNode.next = holdingNode;
+
+    this.length++;
+  }
 
   remove() {}
+
+  //   PrivateHelperMethod
+  _traverseToIndex(index) {
+    let count = 0;
+    let currentNode = this.head;
+
+    while (count !== index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+    return currentNode;
+  }
 
   print() {
     const arr = [];
@@ -48,8 +106,14 @@ class LinkedList {
 
 const linkedList = new LinkedList();
 
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
+linkedList.append(0).append(1).append(2).append(3);
+
+// linkedList.insert(2, 200);
+
+// linkedList.prepend(10);
+// linkedList.prepend(20);
+// linkedList.prepend(30);
+
+// linkedList.insert(2, 100);
 
 linkedList.print();
