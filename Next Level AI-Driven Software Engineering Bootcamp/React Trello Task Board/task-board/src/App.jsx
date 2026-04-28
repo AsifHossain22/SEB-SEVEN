d0f08c718e47e5a6c9d94c5edfe03c834cb08d79
AsifHossain22 <only1asif4you22@gmail.com>
2026-04-28 11:46:45 +0400

    Project set-up for 'React Trello Task Board using Vite React, TailwidCSS, Lucide Icons'

---
+++ Next Level AI-Driven Software Engineering Bootcamp/React Trello Task Board/task-board/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      {/* Header */}
+      <header>
+        <h1 className="text-5xl text-yellow-500 font-bold text-center mt-50">
+          Trello Task Board
+        </h1>
+      </header>
+
+      {/* MainContent */}
+      <main></main>
+
+      {/* Footer */}
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
