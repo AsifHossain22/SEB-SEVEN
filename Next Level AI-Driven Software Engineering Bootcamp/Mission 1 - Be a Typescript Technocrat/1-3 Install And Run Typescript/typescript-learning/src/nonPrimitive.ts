b817699c817c23e6cae9775d02885b7246a7f7b7
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-01 15:13:56 +0400

    Explored 'Object, LiteralType, OptionnalProperty, AccessModifier' in Typescript

--- Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/1-3 Install And Run Typescript/typescript-learning/src/nonPrimitive.ts
+++ Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/1-3 Install And Run Typescript/typescript-learning/src/nonPrimitive.ts
@@ -3,7 +3,6 @@
 /*
  * JS:
  * Array
- * Object
  *
  * TS:
  * Tuple
@@ -23,3 +22,48 @@ let AsifNameAndRoll: [string, number] = ["Asif", 22];
 AsifNameAndRoll[0] = 22;
 
 let destination: [string, string, number] = ["Dhaka", "Rajshahi", 3];
+
+/*
+ * JS:
+ * Object (ReferenceType)
+ *
+ * TS:
+ * Tuple
+ */
+
+const user: { firstName: string; middleName: string; lastName: string } = {
+  firstName: "Asif",
+  middleName: "Hossain",
+  lastName: "Ratul",
+};
+
+const user2: {
+  // organization: string;
+  organization: "Programming Hero"; // LiteralType - ValueAsType
+  firstName: string;
+  middleName?: string; // OptionalProperty
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "Programming Hero",
+  firstName: "Asif",
+  lastName: "Hossain",
+  isMarried: true,
+};
+user2.organization = "Next Level Web Development";
+console.log(user2);
+
+const user3: {
+  readonly organization: string; // AccessModifier
+  // organization: "Programming Hero"; // LiteralType - ValueAsType
+  firstName: string;
+  middleName?: string; // OptionalProperty
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "Programming Hero",
+  firstName: "Asif",
+  lastName: "Hossain",
+  isMarried: true,
+};
+user3.organization = "Next Level Web Development";
