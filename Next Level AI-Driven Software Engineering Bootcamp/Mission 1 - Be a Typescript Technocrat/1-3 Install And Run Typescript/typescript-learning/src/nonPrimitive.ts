d0c30f44419ad55ad9cd981da87f7310973092fe
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-01 09:08:01 +0400

    Explored Non Primitive Types

---
+++ Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/1-3 Install And Run Typescript/typescript-learning/src/nonPrimitive.ts
@@ -0,0 +1,25 @@
+// JS - TS
+
+/*
+ * JS:
+ * Array
+ * Object
+ *
+ * TS:
+ * Tuple
+ */
+
+let bazarList: string[] = ["eggs", "milk", "sugar"];
+
+bazarList.push(7);
+
+let mixedArray: (string | number)[] = ["eggs", 12, "milk", 1, "sugar", 2];
+
+let coordinates: [number, number] = [20, 30, 50];
+
+let couple: [string, string] = ["Husband", "Wife"];
+
+let AsifNameAndRoll: [string, number] = ["Asif", 22];
+AsifNameAndRoll[0] = 22;
+
+let destination: [string, string, number] = ["Dhaka", "Rajshahi", 3];
