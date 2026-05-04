89f987bc1eea9c74cc654a0a36f63f64277049c3
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-04 18:11:34 +0400

    Resolved 'Practice Task 2'

--- Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/typescript-learning/src/typeScriptMasteryTasks.ts
+++ Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/typescript-learning/src/typeScriptMasteryTasks.ts
@@ -10,17 +10,50 @@ function calculateTotal({ price, quantity = 1 }: CartItem) {
   return price * quantity;
 }
 // Test1
-const result1 = calculateTotal({
+const task1result1 = calculateTotal({
   name: "মারহাবা, জাভাস্ক্রিপ্টে মারো থাবা",
   price: 500,
   quantity: 2,
 });
 
 // Test2
-const result2 = calculateTotal({
+const task1result2 = calculateTotal({
   name: "Programming TypeScript",
   price: 1000,
   quantity: 2,
 });
 
-console.log(`Test 1: ${result1}, Test 2: ${result2}`);
+console.log(
+  `Task 1 - Result 1: ${task1result1}, Task 2 - Result 2: ${task1result2}`,
+);
+
+//* Task 2: The "Optional" Shopping Cart
+
+type Person = { name: string; age: number };
+type JobDetails = { role: string; salary: number };
+
+type Employee = Person & JobDetails;
+
+const employee1: Employee = {
+  name: "ASIF",
+  age: 25,
+  role: "Web Developer",
+  salary: 1500,
+};
+
+const employee2: Employee = {
+  name: "JOHORA",
+  age: 25,
+  role: "Problem Solver",
+  salary: 3000,
+};
+
+function getProfile(employee: Employee): string {
+  return `Name: ${employee.name}, Age: ${employee.age}, Role: ${employee.role} & Salary: ${employee.salary}`;
+}
+const task2result1 = getProfile(employee1);
+const task2result2 = getProfile(employee2);
+
+console.log(
+  `Task 2 - Result 1: ${task2result1}, Task 2 - Result 2: ${task2result2}`,
+);
