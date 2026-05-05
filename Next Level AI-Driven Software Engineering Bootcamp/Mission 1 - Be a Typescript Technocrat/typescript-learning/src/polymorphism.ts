13603d5db99058b3e8390c149d17cc7beaf3b20e
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-05 23:40:24 +0400

    Explored 'Polymorphism - OOP' in Typescript

---
+++ Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/typescript-learning/src/polymorphism.ts
@@ -0,0 +1,31 @@
+// Polymorphism - OOP
+
+class Person {
+  getSleep() {
+    console.log("Sleep for 6-8 hrs");
+  }
+}
+
+class Student extends Person {
+  getSleep() {
+    console.log("I sleep for 4-5 hrs");
+  }
+}
+
+class NextLevelDeveloper extends Person {
+  getSleep() {
+    console.log("Next Level Developer sleep for 6 hrs");
+  }
+}
+
+const getSleepingHours = (param: Person) => {
+  param.getSleep();
+};
+
+const person1 = new Person();
+const person2 = new Student();
+const person3 = new NextLevelDeveloper();
+
+getSleepingHours(person1);
+getSleepingHours(person2);
+getSleepingHours(person3);
