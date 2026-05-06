40959660c856e0f4da5d160ea2f1ed853ebe094d
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-06 11:10:51 +0400

    Explored 'Abstraction - OOP' in Typescript

---
+++ Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/typescript-learning/src/abstraction.ts
@@ -0,0 +1,49 @@
+//? Abstraction - OOP
+
+/*
+// * Using - Interface
+interface MediaPlayer {
+  play(): void;
+  pause(): void;
+  stop(): void;
+}
+
+class MusicPlayer implements MediaPlayer {
+  play() {
+    console.log("Play the music");
+  }
+  pause() {
+    console.log("Pause the music");
+  }
+  stop() {
+    console.log("Stop the music");
+  }
+}
+
+const MyMusicPlayer = new MusicPlayer();
+MyMusicPlayer.play();
+*/
+
+//* Using - Abstract
+abstract class AbstractMediaPlayer {
+  abstract play(): void;
+  abstract pause(): void;
+  abstract stop(): void;
+}
+
+class MyAbstractMediaPlayer extends AbstractMediaPlayer {
+  play(): void {
+    console.log("Play 'Abstract' music");
+  }
+  pause(): void {
+    console.log("Pause 'Abstract' music");
+  }
+  stop(): void {
+    console.log("Stop 'Abstract' music");
+  }
+}
+
+const MyAbstractPlayer1 = new MyAbstractMediaPlayer();
+MyAbstractPlayer1.play();
+MyAbstractPlayer1.pause();
+MyAbstractPlayer1.stop();
