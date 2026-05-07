af7615697ca203816a2b4dbce88835cbc1ef72ff
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-07 11:23:52 +0400

    Resolved: 'Problem 2'

--- Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/Assignment/b7a1/solutions.ts
+++ Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/Assignment/b7a1/solutions.ts
@@ -6,3 +6,11 @@ function filterEvenNumbers(numbers: number[]): number[] {
 }
 const onlyEvenNumbers = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 console.log(onlyEvenNumbers);
+
+//? Problem - 2
+function reverseString(wordString: string): string {
+  const reversedWord = wordString.split("").reverse().join("");
+  return reversedWord;
+}
+const returnReverseString = reverseString("typescript");
+console.log(returnReverseString);
