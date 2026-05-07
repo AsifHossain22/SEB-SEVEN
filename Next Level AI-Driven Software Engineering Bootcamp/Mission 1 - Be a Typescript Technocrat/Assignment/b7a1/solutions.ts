11c74105bd40963eb07625c4cbbe292db6775895
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-07 12:10:42 +0400

    Resolved: 'Problem 3'

--- Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/Assignment/b7a1/solutions.ts
+++ Next Level AI-Driven Software Engineering Bootcamp/Mission 1 - Be a Typescript Technocrat/Assignment/b7a1/solutions.ts
@@ -14,3 +14,18 @@ function reverseString(wordString: string): string {
 }
 const returnReverseString = reverseString("typescript");
 console.log(returnReverseString);
+
+//? Problem - 3
+type TStringOrNumber = string | number;
+
+function checkType(whatIsTheType: TStringOrNumber) {
+  if (typeof whatIsTheType === "string") {
+    console.log("This is String Type");
+  } else if (typeof whatIsTheType === "number") {
+    console.log("This is Number Type");
+  } else {
+    console.log("This isn't a String or Number Type");
+  }
+}
+checkType("Hello");
+checkType(7);
