5cf3fe0115e4687705c576d71e242581ee03ef80
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-11 11:34:38 +0400

    Explored 'Route Handling with HTTP Requests & Responses'

--- learningNode/src/server.ts
+++ learningNode/src/server.ts
@@ -1,7 +1,24 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server: Server = createServer((req: IncomingMessage, res) => {
-  console.log(req);
+  //   console.log(req.url); // "/, /user, /products"
+  //   console.log(req.method); // "GET, POST, PATCH, PUT, DELETE"
+
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
+    // console.log("This is ROOT route");
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "This is Root route" }));
+  } else if (url?.startsWith("/products")) {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "This is Product route" }));
+  } else {
+    res.writeHead(404, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "This Route isn't found!" }));
+  }
 });
 
 server.listen(5000, () => {
