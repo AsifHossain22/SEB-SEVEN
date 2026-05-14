0594352346ccc9acb3bb42de4ed22ab896dfdcf9
AsifHossain22 <only1asif4you22@gmail.com>
2026-05-14 11:36:46 +0400

    Executing Pool and creating tables

--- learningExpress/src/server.ts
+++ learningExpress/src/server.ts
@@ -19,6 +19,29 @@ const pool = new Pool({
     'postgresql://neondb_owner:npg_VK1joI4ESdJr@ep-lingering-hall-aprl9atz-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
 });
 
+// Database - DB
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `);
+    console.log('Database connected successfully!');
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
+
 // GET
 app.get('/', (req: Request, res: Response) => {
   // res.send('Hello Express Server!');
